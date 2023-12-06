f135364535cb2e00761cbffee9bf7c514e422c1c
ada lovelace <williambookman@pursuit.org>
2023-12-05 21:07:45 -0500

    lab completed

---
+++ script.js
@@ -0,0 +1,16 @@
+//Create an array 'fruits' with the names of three different fruits.
+
+const fruits = ['apple', 'banana', 'cherry'];
+console.log(fruits)
+
+//Add two more fruits to the 'fruits' array
+fruits.push('orange', 'grape');
+console.log(fruits);
+
+//Create an array 'numbers' and fill it with five different numbers.
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// Print the first and last item of the 'numbers' array.
+console.log(`${numbers[0]}, ${numbers[numbers.length - 1]}`);
+
