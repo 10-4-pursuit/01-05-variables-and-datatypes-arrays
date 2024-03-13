a83d24588527dbc961166b9e9dc16528ff13f110
dttrue <danieltorres@pursuit.org>
2024-03-13 18:53:44 -0400

    Daniels Var and Data Types: Arrays

---
+++ index.js
@@ -0,0 +1,12 @@
+const fruits = ["apple", "orange", "cherry"]
+
+fruits[3] = 'mango'
+fruits[4] = 'banana'
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(fruits)
+console.log(numbers[0], numbers[4])
+
+
+
